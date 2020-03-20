a2b5d38070f79f5a7b3823dcf9031d31ddacf30d
Karen <k.ortiz026@gmail.com>
2020-03-20 11:55:39 -0400

    Feat: Created portal and fixed welcome screen

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import {StyleSheet} from 'react-native';
 
@@ -8,16 +8,21 @@ import {
   ViroARScene,
   ViroText,
   ViroConstants,
+  ViroAmbientLight,
+  ViroPortal,
+  ViroPortalScene,
+  Viro3DObject,
+  Text,
+  TouchableHighlight,
 } from 'react-viro';
 
 export default class HelloWorldSceneAR extends Component {
-
   constructor() {
     super();
 
     // Set initial state here
     this.state = {
-      text : "Initializing AR..."
+      text: 'Initializing AR ...',
     };
 
     // bind 'this' to functions
@@ -25,19 +30,50 @@ export default class HelloWorldSceneAR extends Component {
   }
 
   render() {
+    // console.log('These are the props on HelloWorldAr', this.props);
     return (
-      <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+      <ViroARScene onTrackingUpdated={this._onInitialized}>
+        <ViroAmbientLight color="#ffffff" intensity={200} />
+        <ViroPortalScene
+          passable={true}
+          dragType="FixedDistance"
+          onDrag={() => {}}>
+          <ViroPortal position={[0, -1, -1.1]} scale={[0.7, 1.4, 0.08]}>
+            <Viro3DObject
+              source={require('./ARportal/portal_wood_frame/portal_wood_frame.vrx')}
+              resources={[
+                require('./ARportal/portal_wood_frame/portal_wood_frame_diffuse.png'),
+                require('./ARportal/portal_wood_frame/portal_wood_frame_normal.png'),
+                require('./ARportal/portal_wood_frame/portal_wood_frame_specular.png'),
+              ]}
+              type="VRX"
+            />
+          </ViroPortal>
+
+        </ViroPortalScene>
+        {/* <Viro360Image source={require("./portal_res/360_island.jpg")} /> */}
+        <ViroText
+          text={this.state.text}
+          scale={[0.5, 0.5, 0.5]}
+          position={[0, 0, -1]}
+          style={styles.helloWorldTextStyle}
+        />
+        {/* <TouchableHighlight
+          // style={localStyles.buttons}
+          onPress={this.props.exitViro}
+          underlayColor={'#68a0ff'}>
+          <Text>Start</Text>
+        </TouchableHighlight> */}
       </ViroARScene>
     );
   }
 
   _onInitialized(state, reason) {
-    if (state == ViroConstants.TRACKING_NORMAL) {
+    if (state === ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text : "Hello World!"
+        text: '',
       });
-    } else if (state == ViroConstants.TRACKING_NONE) {
+    } else if (state === ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
     }
   }
@@ -49,7 +85,7 @@ var styles = StyleSheet.create({
     fontSize: 30,
     color: '#ffffff',
     textAlignVertical: 'center',
-    textAlign: 'center',  
+    textAlign: 'center',
   },
 });
 
