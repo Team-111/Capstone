ae2cc0ef8f05e867c1c123a64ce0affc7994f1cb
Rachel Burrous <rachelashleyburrous@gmail.com>
2020-03-20 23:53:33 -0400

    timer

---
+++ timer/tubeclock.js
@@ -0,0 +1,31 @@
+var h1 = document.getElementsByTagName('h1')[0],
+  start = document.getElementById('start'),
+  seconds = 0,
+  minutes = 0,
+  hours = 0,
+  t;
+
+function add() {
+  seconds++;
+  if (seconds >= 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes >= 60) {
+      minutes = 0;
+      hours++;
+    }
+  }
+
+  h1.textContent =
+    (hours ? (hours > 9 ? hours : '0' + hours) : '00') +
+    ':' +
+    (minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00') +
+    ':' +
+    (seconds > 9 ? seconds : '0' + seconds);
+
+  timer();
+}
+function timer() {
+  t = setTimeout(add, 1000);
+}
+timer();
