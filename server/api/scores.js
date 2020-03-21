739c4d94d0cb111ce3006aabf0d79bd42d6d8fcb
Danielle Kogan <kogandanielle@gmail.com>
2020-03-21 17:02:37 -0400

    Firestore ACCESSED

--- server/api/scores.js
+++ server/api/scores.js
@@ -1,12 +1,12 @@
 const db = require('../db/firebase')
 
+
+
 export async function getScores(req, res) {
   let allScores = db.collection('scores');
   try {
-    let highScores = [];
     let scores = await allScores.get()
-    scores.forEach(doc => highScores.push(doc.data()))
-    res.send(highScores)
+    scores.forEach(doc => console.log(doc.data()))
   } catch (error) {
     console.log(error)
   }
