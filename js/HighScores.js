da5964a539f7073c234983869243a4c81262db3c
Karen <k.ortiz026@gmail.com>
2020-03-23 00:11:53 -0400

    Feat: Fixed the highscores view

--- js/HighScores.js
+++ js/HighScores.js
@@ -1,66 +1,76 @@
 'use strict';
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import {  AppRegistry,
+import {
+  AppRegistry,
   Text,
   View,
   StyleSheet,
   Button,
   PixelRatio,
-  TouchableHighlight,} from 'react-native';
+  TouchableHighlight,
+  Alert,
+} from 'react-native';
 
-import {
-  ViroARScene,
-  ViroText,
-  ViroFlexView,
-  Viro360Image,
-} from 'react-viro';
+import {ViroARScene, ViroText, ViroFlexView, Viro360Image} from 'react-viro';
 
-import {getScores} from '../server/api/scores'
+import {getScores} from '../server/api/scores';
 
 export default class HighScores extends Component {
-
   constructor() {
     super();
 
     this.state = {
-      hiScores: []
-    } // Set initial state here
+      hiScores: [],
+    }; // Set initial state here
   }
   componentDidMount() {
     //callback func for getScores takes in the mapped array of objects from api/scores
-    getScores((inputArr) => {
-      this.setState({hiScores: inputArr})
-    })
+    getScores(inputArr => {
+      this.setState({hiScores: inputArr});
+    });
   }
 
   render() {
     return (
-        <View style={styles.highScoreStyle}>
-          <Text>High Scores:</Text>
-          {this.state.hiScores.map(element => {
-            return (<View key={element.id}><Text>{element.user}, {(element.score).toString()}</Text></View>)
-          })}
-          <Button title="back">Back</Button>
-        </View>
-
-
+      <View style={styles.highScoreStyle}>
+        <Text style={styles.hsTitle}>High Scores</Text>
+        {this.state.hiScores.map(element => {
+          return (
+            <View key={element.id}>
+              <Text style={styles.score}>
+                {element.user} {element.score.toString()}
+              </Text>
+            </View>
+          );
+        })}
+        <Button title="back" onPress={() => this.props.exitViro()}>
+          Back
+        </Button>
+      </View>
     );
   }
-
 }
 
 var styles = StyleSheet.create({
   highScoreStyle: {
     flex: 1,
     flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: 'white',
-    fontFamily: 'Arial',
-    fontSize: 60,
+    //fontFamily: 'Arial',
+    // fontSize: 60,
     color: '#FF9800',
-    textAlignVertical: 'center',
-    textAlign: 'center',
+    // textAlignVertical: 'center',
+    // textAlign: 'center',
+  },
+  hsTitle: {
+    fontSize: 58,
+  },
+  score: {
+    fontSize: 22,
   },
 });
 
--- server/api/scores.js
+++ server/api/scores.js
@@ -1,18 +1,20 @@
-const db = require('../db/firebase')
-
-
+const db = require('../db/firebase');
 
 export async function getScores(callbackFunc) {
   let allScores = db.collection('scores');
   try {
-    let scores = await allScores.get()
+    let scores = await allScores.get();
     //create an array of objects. We run this new array through callbackFunc
-    callbackFunc(scores.docs.map(element => element.data()))
+    //Need to get id and data for each element.
+    callbackFunc(
+      scores.docs.map(element => {
+        return {id: element.id, ...element.data()};
+      }),
+    );
     // scores.forEach(doc => callbackFunc(doc.data()))
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
-
 
 // module.exports = getScores
