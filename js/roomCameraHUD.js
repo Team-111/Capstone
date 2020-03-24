066209e4e7771a5bbf9124ad3fa38b6015c6d4d0
Danielle Kogan <kogandanielle@gmail.com>
2020-03-23 20:24:42 -0400

    Feat: Rough Inventory Structural Layout

--- js/roomCameraHUD.js
+++ js/roomCameraHUD.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import {StyleSheet} from 'react-native';
 
@@ -9,16 +9,62 @@ import {
   ViroBox,
   ViroNode,
   ViroCamera,
-  ViroImage
+  ViroImage,
 } from 'react-viro';
-import TimerComponent from '../js/timer/timerComponent'
 
-const RoomCamera = props => {
-  return (
-    <ViroCamera position={[0,0,0]} active={props.isActive} >
-      <TimerComponent />
-    </ViroCamera>
-  )
+import TimerComponent from '../js/timer/timerComponent';
+import InventoryContainer from '../js/Inventory/inventoryContainer'
+
+class RoomCamera extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inventory: [
+        [[], [], [], [], []],
+        [[], [], [], [], []],
+        [[], [], [], [], []],
+      ],
+      currentInventoryPage: 0,
+    };
+  }
+
+  collectItem(item) {
+    if (this.state.inventory[this.state.currInventoryPage].length < 5) {
+      let updatedInventory = [...this.state.inventory]
+
+      this.setState({inventory: []})
+    }
+
+  }
+
+  changeInventoryPage(inputDirection) {
+    if (inputDirection === 'left') {
+      if (this.state.currentInventoryPage === 0)
+        this.setState({currentInventoryPage: 2});
+      else
+        this.setState({
+          currentInventoryPage: this.state.currentInventoryPage - 1,
+        });
+    } else if (inputDirection === 'right') {
+      if (this.state.currentInventoryPage === 2)
+        this.setState({currentInventoryPage: 0});
+      else
+        this.setState({
+          currentInventoryPage: this.state.currentInventoryPage + 1,
+        });
+    }
+  }
+
+  render() {
+    return (
+      <ViroCamera position={[0, 0, 0]} active={this.props.isActive}>
+        <TimerComponent />
+        <ViroNode position={[0, -0.5, -1.5]} scale={[.5, .5, .5]}>
+        <InventoryContainer currPage = {this.state.inventory[this.state.currentInventoryPage]}/>
+        </ViroNode>
+      </ViroCamera>
+    );
+  }
 }
 
 export default RoomCamera;
