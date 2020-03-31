c76ff403d9b03bc048312a30a769f38d60aae3ce
Danielle Kogan <kogandanielle@gmail.com>
2020-03-31 14:01:25 -0400

    score submitted to database in milliseconds

--- js/reactNativeForms/Winner.js
+++ js/reactNativeForms/Winner.js
@@ -20,20 +20,53 @@ class Winner extends Component {
     this.handleSubmit = this.handleSubmit.bind(this)
   }
   async handleSubmit() {
-    await this.props.submitScore(this.props.currentUser.username, this.props.currentGame.currentTime.min)
+    const timeInMilliseconds = (inputGame) => {
+      let minAsSec = inputGame.currentTime.min * 60;
+      let minAndSec = minAsSec + inputGame.currentTime.sec;
+      return minAndSec * 1000;
+    }
+    await this.props.submitScore(this.props.currentUser.username.split('@')[0], timeInMilliseconds(this.props.currentGame))
     this.props.exitViro('highScores')
   }
   render() {
     return(
-      <View>
-        <Text>{`Congratulations, ${this.props.currentUser.username}`}</Text>
-        <Text>{`You got a highscore of: ${this.props.currentGame.currentTime.min}: ${this.props.currentGame.currentTime.sec}`}</Text>
-        <Text>Rachel, Lauren, Karen, and Danielle are so proud of you.</Text>
-        <Button title="Submit Your Score" onPress={this.handleSubmit}></Button>
+      <View style={styles.bigContainer}>
+        <View style={styles.container}>
+          <Text style={styles.bigCongrats}>{`Congratulations, ${this.props.currentUser.username.split('@')[0]}`}</Text>
+          <Text style={styles.littleCongrats}>{`You got a highscore of: ${this.props.currentGame.currentTime.min}: ${this.props.currentGame.currentTime.sec}`}</Text>
+          <Text style={styles.littleCongrats}>Rachel, Lauren, Karen, and Danielle are so proud of you.</Text>
+          <Button title="Submit Your Score" onPress={this.handleSubmit}></Button>
+        </View>
       </View>
     )
   }
 }
+const styles = StyleSheet.create({
+  bigContainer: {
+    backgroundColor: '#000000'
+  },
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    marginBottom: 400,
+    padding: 20,
+    backgroundColor: '#000000'
+  },
+  bigCongrats: {
+    color: '#ff0000',
+    backgroundColor: '#000000',
+    textAlign: 'center',
+    fontSize: 30,
+    paddingBottom: 10,
+  },
+  littleCongrats: {
+    color: '#ff0000',
+    backgroundColor: '#000000',
+    textAlign: 'center',
+    fontSize: 10,
+    paddingBottom: 10,
+  },
+});
 
 const mapStateToProps = state => ({
   currentGame: state.game,
@@ -45,3 +78,4 @@ const mapDispatchToProps = dispatch => ({
 })
 
 module.exports = connect(mapStateToProps, mapDispatchToProps)(Winner);
+
